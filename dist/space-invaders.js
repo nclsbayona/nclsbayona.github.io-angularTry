5fd1044fe942c8189bfe9c2167cd157c179ec247
nclsbayona <nbayonaord@gmail.com>
2022-02-21 11:05:00 -0500

    Fixes

--- dist/space-invaders.js
+++ dist/space-invaders.js
@@ -1,5 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+import './style.css';
 
 // Setup
 
