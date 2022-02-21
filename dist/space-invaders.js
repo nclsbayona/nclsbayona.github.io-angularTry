19ffa9bddf0ccefb4fdf99ba7058c066233ae157
Nicolas Bayona <bayona.n@javeriana.edu.co>
2022-02-20 20:20:48 -0500

    Update space-invaders.js

--- dist/space-invaders.js
+++ dist/space-invaders.js
@@ -1,6 +1,5 @@
-import "./style.css";
-import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 
