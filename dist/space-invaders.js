c762233f5bf577094abc027071b0bb33bc1c7ea6
Nicolas Bayona <nbayonaord@gmail.com>
2022-02-21 13:00:11 -0500

    Added CSS

--- dist/space-invaders.js
+++ dist/space-invaders.js
@@ -1,7 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
 import './style.css';
-
 // Setup
 
 const scene = new THREE.Scene();
