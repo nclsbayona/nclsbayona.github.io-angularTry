aff83a0325450e1440c36925a960e9fe11151858
Nicolas Bayona <nbayonaord@gmail.com>
2022-02-21 19:58:41 -0500

    THIS

--- dist/space-invaders.js
+++ dist/space-invaders.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
-
+import './style.css';
 // Setup
 
 const scene = new THREE.Scene();
@@ -169,7 +169,7 @@ cameraMove();
 
 function animate() {
   requestAnimationFrame(animate);
-  controls.update();
+  console.log(controls.update());
   renderer.render(scene, camera);
 }
 
