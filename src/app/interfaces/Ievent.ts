91cf3517dd8c988113240d54cb2009471df0a3bd
nclsbayona <nbayonaord@gmail.com>
2022-06-17 21:14:24 -0500

    .

--- src/app/interfaces/Ievent.ts
+++ src/app/interfaces/Ievent.ts
@@ -1,8 +1,10 @@
+import { Ilabel } from "./Ilabel";
+
 export interface Ievent {
     title: string;
     date: string;
     description: string;
     image: string;
     subtitle: string;
-    labels: string[];
+    labels: Ilabel[];
 }
